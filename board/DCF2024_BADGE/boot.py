839d1f2bdd904a9c818a717d108c20280f9b56a5
Naomi Kirby <oskirby@gmail.com>
2024-08-07 14:52:54 -0700

    Add recovery mode by holding both SW4 and SW5 at boot

--- board/DCF2024_BADGE/boot.py
+++ board/DCF2024_BADGE/boot.py
@@ -1,13 +1,18 @@
 # For testing: dump the contents of the initfs.
-import initfs
-import tarfile
-import deflate
-import errno
-import io
 import os
+from machine import Pin
 
-def restore(stream):
+# Restore the initial filesystem.
+def restore():
     print("Restoring initial filesystem")
+
+    import initfs
+    import tarfile
+    import deflate
+    import errno
+    import io
+    import os
+    from machine import Pin
 
     def maybe_mkdir(filename):
         if filename[-1] == '/':
@@ -19,7 +24,7 @@ def restore(stream):
                 return
             raise e
 
-    with deflate.DeflateIO(stream) as gzfile:
+    with deflate.DeflateIO(io.BytesIO(initfs.tarball)) as gzfile:
         tf = tarfile.TarFile(fileobj=gzfile)
         for i in tf:
             # Strip the leading directory.
@@ -35,9 +40,16 @@ def restore(stream):
                 with open(filename, "wb") as outfile:
                     outfile.write(fp.read())
 
-# Automatically restore the filesystem if main.py is missing.
-if "main.py" not in os.listdir("/"):
-    restore(io.BytesIO(initfs.tarball))
+if __name__ == '__main__':
+    # Automatically restore the filesystem if main.py is missing.
+    if "main.py" not in os.listdir("/"):
+        restore()
 
-# Cleanup
-del initfs, tarfile, deflate, errno, io, os, restore
+    # If both SW4 and SW5 are pressed at boot - restore the filesystem.
+    else:
+        sw4 = Pin(10, Pin.IN)
+        sw5 = Pin(11, Pin.IN)
+        if sw4.value() == 0 and sw5.value() == 0:
+            restore()
+
+        del sw4, sw5
