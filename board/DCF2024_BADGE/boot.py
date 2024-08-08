71aa5bc100a67501f0d9a2c72446523b8fc01c28
Naomi Kirby <oskirby@gmail.com>
2024-08-07 19:35:31 -0700

    Add workaround for FAT initialization failure
    I think this is really a bug in Micropython that incorrectly detects
    when the flash needs to be formatted in _boot_fat.py. So let's check
    for such a failure and re-format it ourselves in boot.py

--- board/DCF2024_BADGE/boot.py
+++ board/DCF2024_BADGE/boot.py
@@ -4,15 +4,29 @@ from machine import Pin
 
 # Restore the initial filesystem.
 def restore():
-    print("Restoring initial filesystem")
-
     import initfs
     import tarfile
     import deflate
     import errno
     import io
     import os
+    import vfs
+    import rp2
     from machine import Pin
+
+    # HACK: The RP2 port doesn't reliably initialize the FAT filesystem, if it
+    # looks and smells like the root filesystem doesn't exist then reformat it.
+    if len(os.listdir()) == 0:
+        print("Formatting filesystem")
+        try:
+            vfs.umount("/")
+        except:
+            pass
+
+        bdev = rp2.Flash()
+        vfs.VfsFat.mkfs(bdev)
+        fs = vfs.VfsFat(bdev)
+        vfs.mount(fs, "/")
 
     def maybe_mkdir(filename):
         if filename[-1] == '/':
@@ -24,6 +38,7 @@ def restore():
                 return
             raise e
 
+    print("Restoring initial filesystem:")
     with deflate.DeflateIO(io.BytesIO(initfs.tarball)) as gzfile:
         tf = tarfile.TarFile(fileobj=gzfile)
         for i in tf:
@@ -32,7 +47,7 @@ def restore():
             if len(filename) == 0:
                 continue
 
-            print(filename)
+            print(f"\t{filename}")
             if i.type == tarfile.DIRTYPE:
                 maybe_mkdir(filename)
             else:
